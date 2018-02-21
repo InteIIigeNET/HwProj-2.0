66bef532131c8ce46840940e870fa93a973f146f
MaxVortman <maka.abort@gmail.com>
2018-02-21 11:07:15 +0300

    Added email, tables diagram and some small fix.

--- SqlQuery/HwProj2DBCreateTables.sql
+++ SqlQuery/HwProj2DBCreateTables.sql
@@ -8,7 +8,9 @@ Name nvarchar(30),
 Surname nvarchar(30),
 Gender char,
 UserType char,
+Email varchar(30) not null,
 constraint UQ_Users_NameAndSurname unique (Name, Surname),
+constraint UQ_Users_Email unique (Email),
 constraint PK_Users primary key (Id),
 constraint CK_Users_Gender check (Gender = 'M' or Gender = 'F'),
 constraint CK_Users_UserType check (UserType = 'S' or UserType = 'T')
@@ -17,9 +19,10 @@ constraint CK_Users_UserType check (UserType = 'S' or UserType = 'T')
 go
 
 create table Courses (
-Id uniqueidentifier,
-Name nvarchar(15),
+Id int identity(1,1) not null,
+Name nvarchar(15) not null,
 GroupName nvarchar(15),
+IsComplete bit not null,
 constraint PK_Courses primary key (Id),
 constraint UQ_Courses_Name unique (Name) 
 );
@@ -27,7 +30,7 @@ constraint UQ_Courses_Name unique (Name)
 go
 
 create table CourseMates (
-CourseId uniqueidentifier,
+CourseId int,
 UserId uniqueidentifier,
 constraint FK_CourseMates_CourseId foreign key (CourseId) references Courses (Id) on delete cascade,
 constraint FK_CourseMates_UserId foreign key (UserId) references Users (Id) on delete cascade
@@ -36,10 +39,10 @@ constraint FK_CourseMates_UserId foreign key (UserId) references Users (Id) on d
 go 
 
 create table Tasks (
-Id uniqueidentifier,
-CourseId uniqueidentifier,
+Id int identity(1,1) not null,
+CourseId int,
 Title nvarchar(20),
-Content nvarchar(100),
+Description nvarchar(100),
 constraint PK_Tasks primary key (Id),
 constraint FK_Tasks_CourseId foreign key (CourseId) references Courses (Id) on delete cascade
 );
@@ -47,7 +50,7 @@ constraint FK_Tasks_CourseId foreign key (CourseId) references Courses (Id) on d
 go
 
 create table StudentsHomework (
-TaskId uniqueidentifier,
+TaskId int,
 StudentId uniqueidentifier,
 IsComplete bit not null,
 constraint FK_StudentsHomework_TaskId foreign key (TaskId) references Tasks (Id) on delete cascade,
