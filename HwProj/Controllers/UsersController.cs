072496e42b6628c6e196ce68917a271165582453
Alex Berezhnyh <berejnih.alex2011@yandex.ru>
2018-02-27 23:32:30 +0300

    User fixed

---
+++ HwProj/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HwProj.Models;
+
+namespace HwProj.Controllers
+{
+    public class UsersController : Controller
+    {
+	    [HttpPost]
+	    public bool SignUp(User user)
+	    {
+		    return true;
+	    }
+
+	    [HttpGet]
+	    public bool SignIn()
+	    {
+		    return true;
+	    }
+	    [HttpGet]
+	    public bool Edit()
+	    {
+		    return true;
+	    }
+	}
+}
