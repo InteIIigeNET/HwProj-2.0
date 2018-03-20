10b0f9826f901124a9845811b35e7a1ecb895e87
Dinara Faizullina <imfd@protonmail.com>
2018-03-20 23:02:45 +0300

    Added fonts-awesome icons support

--- HwProj/App_Start/BundleConfig.cs
+++ HwProj/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace HwProj
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/site.css",
+                      "~/Content/fontawesome-all.css"));
 		}
 	}
 }
