69d031ace5943ddcc058825a6463aa157a55ff94
MaxVortman <maka.abort@gmail.com>
2018-03-21 22:11:59 +0300

    Added HomeworkController.

---
+++ HwProj/Controllers/HomeworkController.cs
@@ -0,0 +1,18 @@
+﻿using HwProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HwProj.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        
+        public ActionResult Index(Homework homework)
+        {
+            return View(homework);
+        }
+    }
+}
