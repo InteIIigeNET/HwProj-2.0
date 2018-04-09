24cc5f920049d357659d75c2fd90df2c04b82226
Dinara Faizullina <imfd@protonmail.com>
2018-04-09 18:44:09 +0300

    Updated top menu and added profile partial views
    commented lines are temporary

--- HwProj/App_Start/BundleConfig.cs
+++ HwProj/App_Start/BundleConfig.cs
@@ -24,14 +24,14 @@ namespace HwProj
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
+					  //"~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js",
                       "~/Scripts/mdb.js",
                       "~/Scripts/mdb.min.js", 
                       "~/Scripts/popper.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
+					  //"~/Content/bootstrap.css",
 					  "~/Content/site.css",
                       "~/Content/fontawesome-all.css",
                       "~/Content/mdb.css",
