7de39a524f3be8efac2e2ff9ed7f2ece9c7936ec
MaxVortman <maka.abort@gmail.com>
2018-04-09 23:00:41 +0300

    ++

--- HwProj/Models/CourseMates.cs
+++ HwProj/Models/CourseMates.cs
@@ -10,6 +10,7 @@ namespace HwProj.Models
     [Table("CourseMates")]
     public class CourseMate
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
         public long UserId { get; set; }
