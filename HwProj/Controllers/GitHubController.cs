e766df1e1bfe9f7959ab459da4f8bfdd472c721d
MaxVortman <maka.abort@gmail.com>
2018-05-28 02:58:40 +0300

    Decomposition.

--- HwProj/Controllers/GitHubController.cs
+++ HwProj/Controllers/GitHubController.cs
@@ -12,62 +12,13 @@ using System.Threading.Tasks;
 
 namespace HwProj.Controllers
 {
-    public static class GitHubCreator
-    {
-        private static GitHubRepositoryStorage storageInstance;
-        private static GitHubClient clientInstance;
-
-        public static GitHubRepositoryStorage GetStorageInstance()
-        {
-            if (storageInstance == null)
-            {
-                var token = HttpContext.Current.User.Identity.GetGitHubToken();
-                storageInstance = new GitHubRepositoryStorage(token);
-            }
-            return storageInstance;
-        }
-
-        public static GitHubClient GetClientInstance()
-        {
-            if (clientInstance == null)
-            {
-                var token = HttpContext.Current.User.Identity.GetGitHubToken();
-                clientInstance = new GitHubClient(token);
-            }
-            return clientInstance;
-        }
-    }
-
     [GitHubAccess]
     public class GitHubController : Controller
     {
-	    MainRepository Db = MainRepository.Instance;
-		// GET: GitHub 
-		public async Task<ActionResult> Index()
-        {
-            ViewBag.Repos = await GitHubCreator.GetStorageInstance().GetRepositoriesAsync();
-            return PartialView();
-        }
-
-        [Authorize]
-        [HttpPost]
-        public async Task<ActionResult> Index(PullRequestCreateViewModel pullRequestModel)
-        {
-            return PartialView("PullRequest", (await GitHubCreator.GetClientInstance().
-                GetNewPullRequestManagerAsync(pullRequestModel.Title, pullRequestModel.RepositoryName, 
-                pullRequestModel.HeadBranchName, pullRequestModel.BaseBranchName)).PullRequest);
-        }
-
         public async Task<ActionResult> FillBranch(string repository)
         {
-            var branches = await GitHubCreator.GetStorageInstance().GetBranchesAsync(repository);
+            var branches = await GitHubInstance.GetStorageInstance().GetBranchesAsync(repository);
             return Json(branches, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> SendComment(string content)
-        {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ HwProj/Controllers/PullRequestController.cs
@@ -0,0 +1,41 @@
+﻿using HwProj.Filters;
+using HwProj.Models.ViewModels;
+using HwProj.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HwProj.Controllers
+{
+    [GitHubAccess]
+    public class PullRequestController : Controller
+    {
+        // GET: PullRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chose()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            ViewBag.Repos = await GitHubInstance.GetStorageInstance().GetRepositoriesAsync();
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(PullRequestCreateViewModel pullRequestModel)
+        {
+            return PartialView("Index", (await GitHubInstance.GetClientInstance().
+                GetNewPullRequestManagerAsync(pullRequestModel.Title, pullRequestModel.RepositoryName,
+                pullRequestModel.HeadBranchName, pullRequestModel.BaseBranchName)).PullRequest);
+        }
+    }
+}
