9581a3afbdeaf78718c4226b7c2518f39259d2f8
Alex Berezhnyh <berejnih.alex2011@yandex.ru>
2018-05-30 03:47:30 +0300

    Больше паттернов и маркетинговых ходов.

--- HwProj/Tools/Markdown/MarkdownInterpreter.cs
+++ HwProj/Tools/Markdown/MarkdownInterpreter.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using HeyRed.MarkdownSharp;
+using HwProj.Tools.Markdown;
 
 namespace HwProj.Tools
 {
 	public static class MarkdownInterpreter
 	{
-		static readonly Markdown Engine = new Markdown(new MarkdownOptions
-		{
-			AutoHyperlink = true,
-			AutoNewLines = true,
-			LinkEmails = true,
-			QuoteSingleLine = true
-		});
+		private static IMarkdownInterpreter _interpreter = 
+			new StandartMarkdownInterpreter();
 
 		public static string AsMarkdown(this string text)
 		{
 			try
 			{
-				return Engine.Transform(text);
+				return _interpreter.ConvertToHtml(text);
 			}
 			catch (Exception ex)
 			{
