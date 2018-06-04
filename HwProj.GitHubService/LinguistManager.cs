3a8fbd28521c6a6393fdf9ba0a9d17d126d2f18d
MaxVortman <maka.abort@gmail.com>
2018-06-04 23:43:08 +0300

    LinguistManager implemented.

---
+++ HwProj.GitHubService/LinguistManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HwProj.GitHubService
+{
+    internal static class LinguistManager
+    {
+        private static string XMLFilePath = Path.Combine(Environment.CurrentDirectory, "Linguist.xml");
+        private static XDocument LinguistXMl = XDocument.Load(XMLFilePath);
+
+        public static string GetAlias(string extension)
+        {
+            return (from xLanguage in LinguistXMl.Elements("languages")
+                    where (from xExtension in xLanguage.Elements("extensions")
+                           where xExtension.Value == extension
+                           select extension).Any()
+                    select xLanguage.Element("alias").Value).FirstOrDefault();
+        }
+    }
+}
