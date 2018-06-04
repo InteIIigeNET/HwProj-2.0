0027620a7c0e60354df63fbf6b377c64bd391717
MaxVortman <maka.abort@gmail.com>
2018-06-05 00:13:01 +0300

    One more test.

--- HwProj.GitHubService.Test/LinguistManagerTest.cs
+++ HwProj.GitHubService.Test/LinguistManagerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HwProj.GitHubService;
+using System.IO;
 
 namespace HwProj.GitHubService.Test
 {
@@ -8,11 +9,24 @@ namespace HwProj.GitHubService.Test
     public class LinguistManagerTest
     {
         [TestMethod]
-        public void ShouldReturnCSharp()
+        public void ShouldReturnCSharpFromExtension()
         {
             //arrange
             const string extension = ".cs";
             const string expectedRes = "csharp";
+            //act
+            var res = LinguistManager.GetAlias(extension);
+            //assert
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCPPFromFileName()
+        {
+            //arrange
+            const string fileName = "main.cpp";
+            const string expectedRes = "cpp";
+            var extension = Path.GetExtension(fileName);
             //act
             var res = LinguistManager.GetAlias(extension);
             //assert
