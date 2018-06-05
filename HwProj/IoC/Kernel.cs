9070fcf75a20ee590ceea75ab96068f915b10164
Dinara Faizullina <imfd@protonmail.com>
2018-06-05 18:21:42 +0300

    Index update

--- HwProj/IoC/Kernel.cs
+++ HwProj/IoC/Kernel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using HwProj.Tools;
 using HwProj.Tools.Markdown;
-using IdentityServer4;
 using Ninject;
 
 namespace HwProj.IoC
--- HwProj/Models/ViewModels/TaskViewModel.cs
+++ HwProj/Models/ViewModels/TaskViewModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using IdentityServer4.Models;
 
 namespace HwProj.Models.ViewModels
 {
